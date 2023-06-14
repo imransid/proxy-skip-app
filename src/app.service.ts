aff8ecd838e5da3edd577fb2fffca24667e4e9a2
Imran Khan <109821243+imrancrow93@users.noreply.github.com>
2023-06-14 14:30:28 +0600

    tareq bhai

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Rafa';
+    return 'Hello World! Tareq bhai fuck u';
   }
 }
