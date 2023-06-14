0df780e7dc3fca8055f5ffb76b38b759f2093a38
Imran Khan <109821243+imrancrow93@users.noreply.github.com>
2023-06-14 18:08:52 +0600

    set issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): any {
+  getJira(): any {
     return this.appService.getJira();
+  }
+
+  @Post()
+  createIssue(@Body() createData: object): any {
+    return this.appService.createIssue(createData);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,24 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
+const auth =
+  'Basic TWluaGF6dWwuSGFzYW5AYnJhaW5zdGF0aW9uLTIzLmNvbTpBVEFUVDN4RmZHRjA1SFpvcVBRMFpqaEhqemU2UkdBc1BoSDNycTVJXzFJdjhZa2g4dDJEN053NHdwaWhPalJOX0R0RjlKREhHcmRGSm1ScVBQRW1FdmlWbEtjRE9VS05fWFN1MTFld0QwbXBfYmdlMTc2eElmZTN1aXlnckg0OV8xLWFMLUdUcUhFSTN2SDhyUURxZmxYSnZPakQ3eDRGM18xWjVPTzM3MjFEM2NoaDZubnlGZlk9RjMwRjBBNDA=';
+
 @Injectable()
 export class AppService {
   async getJira(): Promise<any> {
     const url = 'https://ari-us.atlassian.net/rest/agile/1.0/board/3/issue';
     const headers = {
-      Authorization:
-        'Basic TWluaGF6dWwuSGFzYW5AYnJhaW5zdGF0aW9uLTIzLmNvbTpBVEFUVDN4RmZHRjA1SFpvcVBRMFpqaEhqemU2UkdBc1BoSDNycTVJXzFJdjhZa2g4dDJEN053NHdwaWhPalJOX0R0RjlKREhHcmRGSm1ScVBQRW1FdmlWbEtjRE9VS05fWFN1MTFld0QwbXBfYmdlMTc2eElmZTN1aXlnckg0OV8xLWFMLUdUcUhFSTN2SDhyUURxZmxYSnZPakQ3eDRGM18xWjVPTzM3MjFEM2NoaDZubnlGZlk9RjMwRjBBNDA=',
+      Authorization: auth,
       Cookie:
         'atlassian.xsrf.token=6636e62f-8a68-42e7-9f30-6d4eff66797a_9f1c07ec8f6e25584d28b76526517d0e331d19d1_lin',
     };
 
     try {
       const response = await axios.get(url, { headers });
-      //console.log(response.data);
       return response.data;
     } catch (error) {
       console.log('error', error);
       throw error;
     }
+  }
+
+  async createIssue(createData: object): Promise<any> {
+    let data = JSON.stringify(createData);
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://ari-us.atlassian.net/rest/api/2/issue',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: auth,
+        Cookie:
+          'atlassian.xsrf.token=6636e62f-8a68-42e7-9f30-6d4eff66797a_9f1c07ec8f6e25584d28b76526517d0e331d19d1_lin',
+      },
+      data: data,
+    };
+
+    return await axios
+      .request(config)
+      .then((response) => {
+        let data = JSON.stringify(response.data);
+
+        return data;
+      })
+      .catch((error) => {
+        return error;
+      });
   }
 }
